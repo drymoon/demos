8e8c577be30fa869353d710742cce200541ab32d
DryMoon <noahmizrahi@gmail.com>
2015-05-10 14:34:21 -0700

    adding tornado

---
+++ heroku_tornado_test/main.py
@@ -0,0 +1,19 @@
+import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+     
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render("index.html") 
+def main():
+    	application = tornado.web.Application([
+    	(r"/", MainHandler),
+    	])
+    	http_server = tornado.httpserver.HTTPServer(application)
+    	port = int(os.environ.get("PORT", 5001))
+    	http_server.listen(port)
+    	tornado.ioloop.IOLoop.instance().start()
+     
+if __name__ == "__main__":
+    main()
